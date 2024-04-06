f0e15aa11422be4edbbe66f5c860131d20dc66b2
Chandra  Kishor <chandrakishor@Chandras-MacBook-Air.local>
2024-04-06 20:08:08 +0530

    first commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,10 @@
+export const LOGO_URL = 'https://upload.wikimedia.org/wikipedia/en/b/b5/National_Institute_of_Technology%2C_Patna_Logo.png';
+export const BASE_URL =  'https://alumni-track-server.vercel.app/api/v1';
+
+
+
+
+
+
+
+
